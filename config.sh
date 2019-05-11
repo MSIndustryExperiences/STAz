0a59d7f938d831e98ba17725844856bedc29ab74
David Starr <david@elegantcode.com>
2019-05-11 11:00:09 -0700

    got rid of template deployments. Going all CLI.

--- config.sh
+++ config.sh
@@ -8,4 +8,6 @@ PREFIX="st-$CUSTOMER_NAME" #prepended to resources
 
 RESOURCE_GROUP_NAME="$PREFIX-rg"
 
+SUB_ID="ab667571-f9a4-444a-9b43-6e32602a40ba"
+
 VM_ADMIN_UID="hsimpson"
--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,5 @@
 # az login
 
 create_resource_group
-create_vm "$PREFIX-01-vm" "$PREFIX-os-disk" Standard_D4s_v3
+create_vm "$PREFIX-01-vm" "$PREFIX-01-os-disk" Standard_D4s_v3
+
