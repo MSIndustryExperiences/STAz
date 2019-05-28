99cedd83035953cdc1d77ce480367b29cdd2ed7b
David Starr <david@elegantcode.com>
2019-05-28 09:56:27 -0700

    only opening designated ports on PAW machine.

--- templates/validate_templates.sh
+++
@@ -1,8 +0,0 @@
-. ../config/config_test.sh
-
-# validate the nsg provisioning template
-az group deployment validate \
-  --resource-group $RESOURCE_GROUP_NAME \
-  --template-file nsg.json \
-  --parameters @nsg_parameters.json  
-
