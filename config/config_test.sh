855898f4a6026578d1d3d7cc14255d557c2e6455
David Starr <david@elegantcode.com>
2019-06-02 07:12:49 -0700

    killing templates

--- config/config_test.sh
+++ config/config_test.sh
@@ -4,7 +4,7 @@ CUSTOMER_NAME="sytrue"
 LOCATION="westus2" # the region where you want to deploy resources
 VM_ADMIN_UID="cheri"
 
-PREFIX="$CUSTOMER_NAME-23" #prepended to resources
+PREFIX="$CUSTOMER_NAME-01" #prepended to resources
 
 # Networking
 VNET_NAME="$PREFIX-vnet"
