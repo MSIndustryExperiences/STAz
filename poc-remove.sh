ed37fb24ce15c86ddd44a740dc6768beb29e3d9f
David Starr <david@elegantcode.com>
2019-07-08 11:09:30 -0700

    removing poc scripts - not needed

--- poc-remove.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-. ./config/config_poc.sh
-. ./scripts/resource_group.sh
-
-# Sign in on the command line with this command or uncomment this line
-# az login
-
-delete_resource_group
